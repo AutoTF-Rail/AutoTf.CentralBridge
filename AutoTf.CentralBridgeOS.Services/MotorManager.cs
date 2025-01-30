81c0f167e478bbf771a08231d9ab8c5d5835d0de
chiouya <chiouyazo@gmail.com>
2025-01-30 22:54:36 +0100

    hopefully fixed angle bug

--- AutoTf.CentralBridgeOS.Services/MotorManager.cs
+++ AutoTf.CentralBridgeOS.Services/MotorManager.cs
@@ -51,9 +51,9 @@ public class MotorManager : IDisposable
 
 		double pulseWidthMs = MinPulseWidthMs + (angle / 270.0) * (MaxPulseWidthMs - MinPulseWidthMs);
 
-		double dutyCycle = pulseWidthMs / (1000.0 / PwmFrequency) * 100;
-		_logger.Log($"Motor: Setting {channel} to {dutyCycle / 100}");
-		_pca.SetDutyCycle(channel, dutyCycle / 100);
+		double dutyCycle = pulseWidthMs / (1000.0 / PwmFrequency);
+		_logger.Log($"Motor: Setting {channel} to {dutyCycle}");
+		_pca.SetDutyCycle(channel, dutyCycle);
 	}
 	
 	public void MoveToMiddle()
