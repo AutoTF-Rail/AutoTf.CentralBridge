aaf0b13e34f5771c6515880e46ddf0279700fa80
chiouya <chiouyazo@gmail.com>
2025-02-21 15:33:36 +0100

    Fixed hotspot device deletion

--- AutoTf.CentralBridgeOS.Services/NetworkManager.cs
+++ AutoTf.CentralBridgeOS.Services/NetworkManager.cs
@@ -1,7 +1,4 @@
-using System;
-using System.IO;
 using AutoTf.Logging;
-using Timer = System.Timers.Timer;
 
 namespace AutoTf.CentralBridgeOS.Services;
 
@@ -10,21 +7,14 @@ public class NetworkManager : IDisposable
 	private readonly Logger _logger = Statics.Logger;
 	private readonly FileSystemWatcher _watcher = new FileSystemWatcher();
 
+	private HashSet<string> _knownDevices = new HashSet<string>();
+
 	public NetworkManager()
 	{
 		Statics.ShutdownEvent += Dispose;
-		Initialize();
-	}
-	
-	private void Initialize()
-	{
-		// Check for internet
-		// Sync MAC Addresses
-		// Start listening for new devices
-
 		StartConnectionListener();
 	}
-
+	
 	private void StartConnectionListener()
 	{
 		const string leasesFilePath = "/var/lib/misc/dnsmasq.leases";
@@ -32,57 +22,50 @@ public class NetworkManager : IDisposable
 		_watcher.Path = Path.GetDirectoryName(leasesFilePath)!;
 		_watcher.Filter = Path.GetFileName(leasesFilePath);
 		_watcher.Changed += OnLeasesFileChanged;
-		_watcher.Deleted += OnDeleted;
 
 		_watcher.EnableRaisingEvents = true;
 	}
 
-	private void OnDeleted(object sender, FileSystemEventArgs e)
-	{
-		_logger.Log("A device has disconnected from the hotspot.");
-		string[] lines = File.ReadAllLines(e.FullPath);
-		
-		foreach (string line in lines)
-		{
-			// time, MAC, IP, name, id
-			string[] parts = line.Split(' ');
-
-			if (parts.Length <= 1) 
-				continue;
-			
-			_logger.Log("Device:");
-			_logger.Log("MAC: " + parts[1]);
-			_logger.Log("IP: " + parts[2]);
-			_logger.Log("Host: " + parts[3]);
-			if (Statics.AllowedDevices.Contains(parts[1]))
-				Statics.AllowedDevices.Remove(parts[1]);
-		}
-	}
-
 	private void OnLeasesFileChanged(object sender, FileSystemEventArgs e)
 	{
-		_logger.Log("A new device has connected on the hotspot.");
+		if (!File.Exists(e.FullPath))
+		{
+			_logger.Log("Leases file missing. Skipping device update.");
+			return;
+		}
+		_logger.Log("Leases file changed. Checking connected devices...");
+		HashSet<string> currentDevices = new HashSet<string>();
 		string[] lines = File.ReadAllLines(e.FullPath);
 		
 		foreach (string line in lines)
 		{
-			// time, MAC, IP, name, id
 			string[] parts = line.Split(' ');
-
 			if (parts.Length <= 1) 
 				continue;
-			
-			_logger.Log("New device:");
-			_logger.Log("MAC: " + parts[1]);
-			_logger.Log("IP: " + parts[2]);
-			_logger.Log("Host: " + parts[3]);
+        
+			string mac = parts[1];
+			currentDevices.Add(mac);
+        
+			if (!_knownDevices.Contains(mac))
+			{
+				_logger.Log("New device connected:");
+				_logger.Log($"MAC: {mac}");
+				_logger.Log($"IP: {parts[2]}");
+				_logger.Log($"Host: {parts[3]}");
+			}
 		}
-	}
 
-	private void PendingDeviceElapsed(string macAddr)
-	{
-		_logger.Log($"Timer elapsed for {macAddr}. Kicking device from network.");
-		CommandExecuter.ExecuteSilent($"hostapd_cli deauthenticate {macAddr}", true);
+		foreach (string mac in _knownDevices)
+		{
+			if (!currentDevices.Contains(mac))
+			{
+				_logger.Log($"Device disconnected: {mac}");
+				if (Statics.AllowedDevices.Contains(mac))
+					Statics.AllowedDevices.Remove(mac);
+			}
+		}
+
+		_knownDevices = currentDevices;
 	}
 
 	public void Dispose()
